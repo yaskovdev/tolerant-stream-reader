5f6bef800f666c69b06fcefeff31714a761b4ab3
Sergei Iaskov <sergeiiaskov@microsoft.com>
2025-11-17 13:37:27 +0200

    Add initial project files and configuration.

---
+++ TolerantStreamReader/README.md
@@ -0,0 +1,1 @@
+# Tolerant Stream Reader
---
+++ TolerantStreamReader/TolerantStreamReader/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace TolerantStreamReader;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
